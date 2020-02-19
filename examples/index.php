77c6dae300d0233984d06d9654ba981b0741d94a
USUI Tomoaki <usui@lifr21.com>
2020-02-19 16:44:21 +0900

    discovery()、getBaseAuthorizationUrl()、getBaseAccessTokenUrl() を実装

---
+++ examples/index.php
@@ -0,0 +1,87 @@
+<?php
+require_once '../vendor/autoload.php';
+
+use GNOffice\OAuth2\Client\Provider\YahooJapan;
+
+$provider = new YahooJapan([
+    'clientId' => 'dj00aiZpPVFXS1Z1bG9rTEE0MiZzPWNvbnN1bWVyc2VjcmV0Jng9ZjY-',
+    'clientSecret' => 'vrXYgL1ruSTFNPy9XjIEUasJmrdrMWVIVkKE45cv',
+    'redirectUri' => 'http://192.168.56.103/oauth2-yahoo-japan/examples/index.php',
+]);
+
+if (!isset($_GET['code'])) {
+
+    // 
+    $nonce = $this->random(40);
+    $_SESSION['nonce'] = $nonce;
+    $code_verifier = $this->random(100);
+    $_SESSION['code_verifier'] = $code_verifier;
+
+    // code_challenge の作成
+    $hash = hash('sha256', $code_verifier);
+    $code_challenge = $this->base64UrlEncode(pack('H*', $hash));
+    $code_challenge_method = 'S256';
+
+    // Fetch the authorization URL from the provider; this returns the
+    // urlAuthorize option and generates and applies any necessary parameters
+    // (e.g. state).
+    $authorizationUrl = $provider->getAuthorizationUrl();
+
+    // Get the state generated for you and store it to the session.
+    $_SESSION['oauth2state'] = $provider->getState();
+
+    var_dump($authorizationUrl);
+    exit();
+
+    // Redirect the user to the authorization URL.
+    header('Location: ' . $authorizationUrl);
+    exit;
+
+// Check given state against previously stored one to mitigate CSRF attack
+} elseif (empty($_GET['state']) || (isset($_SESSION['oauth2state']) && $_GET['state'] !== $_SESSION['oauth2state'])) {
+
+    if (isset($_SESSION['oauth2state'])) {
+        unset($_SESSION['oauth2state']);
+    }
+
+    exit('Invalid state');
+
+} else {
+
+    try {
+
+        // Try to get an access token using the authorization code grant.
+        $accessToken = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        // We have an access token, which we may use in authenticated
+        // requests against the service provider's API.
+        echo 'Access Token: ' . $accessToken->getToken() . "<br>";
+        echo 'Refresh Token: ' . $accessToken->getRefreshToken() . "<br>";
+        echo 'Expired in: ' . $accessToken->getExpires() . "<br>";
+        echo 'Already expired? ' . ($accessToken->hasExpired() ? 'expired' : 'not expired') . "<br>";
+
+        // Using the access token, we may look up details about the
+        // resource owner.
+        $resourceOwner = $provider->getResourceOwner($accessToken);
+
+        var_export($resourceOwner->toArray());
+
+        // The provider provides a way to get an authenticated API request for
+        // the service, using the access token; it returns an object conforming
+        // to Psr\Http\Message\RequestInterface.
+        $request = $provider->getAuthenticatedRequest(
+            'GET',
+            'http://brentertainment.com/oauth2/lockdin/resource',
+            $accessToken
+        );
+
+    } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+        // Failed to get the access token or user details.
+        exit($e->getMessage());
+
+    }
+
+}

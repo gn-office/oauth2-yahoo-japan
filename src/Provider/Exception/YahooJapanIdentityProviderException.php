c3ee22339d8534c5a89c8fefe926b878296b23b2
USUI Tomoaki <usui@lifr21.com>
2020-02-21 13:55:19 +0900

    例外処理を追加

--- src/Provider/Exception/YahooJapanIdentityProviderException.php
+++ src/Provider/Exception/YahooJapanIdentityProviderException.php
@@ -5,7 +5,42 @@ namespace GNOffice\OAuth2\Client\Provider\Exception;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Psr\Http\Message\ResponseInterface;
 
-class YahooJapanIdentityProviderException
+class YahooJapanIdentityProviderException extends IdentityProviderException
 {
+    public static function clientException(ResponseInterface $response, array $data)
+    {
+        return static::fromResponse(
+            $response,
+            (string)$response->getReasonPhrase()
+        );
+    }
 
+    public static function oauthException(ResponseInterface $response, array $data)
+    {
+        if ($data['error_description']) {
+            $message = $data['error_description'];
+        } else {
+            $message = $data['error'];
+        }
+        return static::fromResponse(
+            $response,
+            $message
+        );
+    }
+
+    public static function invalidTokenException($message = null)
+    {
+        return new \Exception($message);
+    }
+
+    /**
+     * @param  ResponseInterface $response
+     * @param  string|null $message
+     *
+     * @return static
+     */
+    protected static function fromResponse(ResponseInterface $response, $message = null)
+    {
+        return new static($message, $response->getStatusCode(), (string)$response->getBody());
+    }
 }
